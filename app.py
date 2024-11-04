1f3bcabc7c839d6a72bc93cad83c3db44e2aee1d
valencora <coratolovalen@gmail.com>
2024-11-04 13:13:06 -0300

    commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.register_blueprint(mutant_blueprint)
 Base.metadata.create_all(bind=engine)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Usa el puerto que Render proporciona, o 5000 como fallback
+    port = int(os.environ.get('PORT', 5000))  
     app.run(host='0.0.0.0', port=port)
 
 
