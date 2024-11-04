739e7bf87cd932055f93c1233ffa07a8f0421e28
valencora <coratolovalen@gmail.com>
2024-11-04 12:55:56 -0300

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers.controlador import mutant_blueprint
 from database.db_connection import engine
@@ -12,4 +13,7 @@ app.register_blueprint(mutant_blueprint)
 Base.metadata.create_all(bind=engine)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto que Render proporciona, o 5000 como fallback
+    app.run(host='0.0.0.0', port=port)
+
+
